58380ecafd71fa9ccd4c40699ddffdaf907dbdf0
Rowan Cota <cota.rg@gmail.com>
2016-06-13 16:01:54 -0700

    made this an index router.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,8 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+var router = express.Router()
+var Controller = require('../controllers/scrabble')
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', Controller.getIndex)
 
-module.exports = router;
+module.exports = router
