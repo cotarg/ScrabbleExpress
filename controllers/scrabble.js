d67efff94e9daf115289b9ffadb1d114e32f568f
Rowan Cota <cota.rg@gmail.com>
2016-06-14 00:23:31 -0700

    working score page. leaving post method and form to revisit, practiced get with callback patterns.

--- controllers/scrabble.js
+++ controllers/scrabble.js
@@ -3,15 +3,24 @@ var Scrabble = require('../lib/scrabble')
 var ScrabbleController = {
   getIndex: function (req, res, next) {
     res.render('index', { title: 'Express' })
-  }
+  },
 
   getChart: function (req, res) {
     var locals = {}
     var scrabble = new Scrabble()
     locals.points = scrabble.points
-    locals.title = "Scrabble Points Chart of Super Cool Points"
+    locals.title = 'Scrabble Points Chart of Super Cool Points'
 
     res.render('chart', locals)
+  },
+
+  getScore: function (req, res, next) {
+    var locals = {}
+    var scrabble = new Scrabble()
+    locals.word = req.params['word']
+    locals.score = scrabble.score(locals.word)
+
+    res.render('score', locals)
   }
 }
 
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,15 @@
 var express = require('express')
 var router = express.Router()
 var Controller = require('../controllers/scrabble')
+// var Scrabble = require('../lib/scrabble')
 
 /* GET home page. */
 router.get('/', Controller.getIndex)
 
 /* GET chart page */
-router.get('/chart', Controller.getChart)
+router.get('/scrabble/chart', Controller.getChart)
+
+/* GET request for a score for a word */
+router.get('/scrabble/score/:word', Controller.getScore)
 
 module.exports = router
